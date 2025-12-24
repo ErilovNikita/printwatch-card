650a7cbcc3a191c0182a555798d57ac44bb92561
Ерилов Никита <nerilov@octopeople.net>
2025-12-24 23:57:17 +0300

    Add conditions for print_length and print_weight

--- src/templates/components/print-status.js
+++ src/templates/components/print-status.js
@@ -36,10 +36,10 @@ export const printStatusTemplate = (entities, config) => {
         <div class="print-details">
           <h3>${entities.taskName}</h3>
           <div class="print-stats">
-            ${entities.print_length_entity !== undefined ? html`
+            ${entities.print_length_entity && entities.print_length_entity !== undefined ? html`
               ${localize.t('print.length')}: ${entities.print_length_entity} ${config.hass.states[entities.print_length_entity]?.attributes?.unit_of_measurement || ''} |
             ` : ''}
-            ${entities.print_weight_entity !== undefined ? html`
+            ${entities.print_weight_entity && ntities.print_weight_entity !== undefined ? html`
               ${localize.t('print.weight')}: ${entities.print_weight_entity} ${config.hass.states[entities.print_weight_entity]?.attributes?.unit_of_measurement || ''}
             ` : ''}
           </div>
