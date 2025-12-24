28595dc45c3db1077244196f443ccd25fb994ac5
Ерилов Никита <nerilov@octopeople.net>
2025-12-25 00:03:23 +0300

    Add conditions for light

--- src/templates/components/header.js
+++ src/templates/components/header.js
@@ -26,12 +26,14 @@ export const headerTemplate = (entities, controls) => html`
       ` : ''}
     </div>
     <div class="header-controls">
-      <button 
-        class="icon-button ${controls.lightState === 'on' ? 'active' : ''}" 
-        @click=${controls.onLightToggle}
-      >
-        <ha-icon icon="mdi:lightbulb"></ha-icon>
-      </button>
+      ${entities.chamber_light_entity ? html`
+        <button 
+          class="icon-button ${controls.lightState === 'on' ? 'active' : ''}" 
+          @click=${controls.onLightToggle}
+        >
+          <ha-icon icon="mdi:lightbulb"></ha-icon>
+        </button>
+      ` : ''}
       ${entities.aux_fan_entity ? html`
         <button 
           class="icon-button ${controls.fanState === 'on' ? 'active' : ''}"
